93ab56b377164f50e915ba8bdc066ee1e2f67f18
Frederick Lawson <freddie@corneliajames.com>
2024-07-25 09:37:59 -0300

    fixes for client console errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
-/** @type {import('next').NextConfig} */
 import withPWA from 'next-pwa';
 
 const nextConfig = withPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  sw: 'worker.js',
 });
 
 export default nextConfig;
